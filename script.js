46d2cdeab0480d1604b5225cb37515cbcd3b742b
Valentyn Bobyr <valko_bob@ukr.net>
2025-01-21 21:40:37 +0200

    #3

--- script.js
+++ script.js
@@ -4,6 +4,10 @@ const resultEl = document.getElementById('result');
 button.addEventListener('click', async () => {
   const devices = await navigator.mediaDevices.enumerateDevices();
   const filteredData = devices.filter(item => item.kind === 'videoinput');
+  await navigator.mediaDevices.getUserMedia({
+    video: { facingMode: "environment" },
+    audio: false
+  })
 
   resultEl.innerHTML = JSON.stringify(filteredData);
 });
