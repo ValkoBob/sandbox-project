9bd2486cecd2376b145ddce6a80611dd771e6d88
Valentyn Bobyr <valko_bob@ukr.net>
2025-01-22 17:32:57 +0200

    #5

--- script.js
+++ script.js
@@ -2,10 +2,12 @@ const button = document.getElementById('button');
 const resultEl = document.getElementById('result');
 
 button.addEventListener('click', async () => {
-  await navigator.mediaDevices.getUserMedia({
+  const stream = await navigator.mediaDevices.getUserMedia({
     video: { facingMode: "environment" },
     audio: false
-  })
+  });
+
+  stream.play();
 
   const devices = await navigator.mediaDevices.enumerateDevices();
   const filteredData = devices.filter(item => item.kind === 'videoinput');
