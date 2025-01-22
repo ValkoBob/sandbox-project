67589ab3dd6afb6848d47930a1a13821eeab8dfd
Valentyn Bobyr <valko_bob@ukr.net>
2025-01-22 18:10:40 +0200

    #12

--- script.js
+++ script.js
@@ -3,18 +3,16 @@ const resultEl = document.getElementById('result');
 const result2El = document.getElementById('result2');
 
 button.addEventListener('click', async () => {
-  const stream = await navigator.mediaDevices.getUserMedia({
+  /*const stream = */await navigator.mediaDevices.getUserMedia({
     video: { facingMode: "environment" },
     audio: false
+  }).then(async () => {
+    const devices = await navigator.mediaDevices.enumerateDevices();
+    const filteredData = devices.filter(item => item.kind === 'videoinput');
+
+    resultEl.innerHTML = JSON.stringify(filteredData);
   });
 
-  const devices = await navigator.mediaDevices.enumerateDevices();
-  const filteredData = devices.filter(item => item.kind === 'videoinput');
 
-  console.log(devices);
-
-  console.log(stream.getTracks());
-
-  resultEl.innerHTML = JSON.stringify(filteredData);
-  result2El.innerHTML = JSON.stringify(stream.getTracks().map(track => track.label).join(', '));
+  //result2El.innerHTML = JSON.stringify(stream.getTracks().map(track => track.label).join(', '));
 });
